321294ac6b5a4214a32b921b035b2b7df3d1f5ba
vitor7001 <vitorgomes7001@gmail.com>
2020-01-05 12:29:10 -0300

    carregando comentários

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App'
 import {shallow} from 'enzyme'
 import Comments from './Comments'
 import NewComment from './NewComment'
+import { EventEmitter} from 'events'
 
 describe('<App />' , () =>{
   it('renderizar sem erros', () =>{
@@ -18,7 +19,6 @@ describe('<App />' , () =>{
     expect(wrapper.find(NewComment).length).toBe(1)
     expect(wrapper.find('p').length).toBe(1)
   })
-
 
   it('adicionar um novo comentário', () =>{
     const database = {
@@ -45,4 +45,40 @@ describe('<App />' , () =>{
       'comments/1' : {'comment' : 'new comment'}
     })
   })
+
+  it('renderizar comentários do firebase', () =>{
+    const database = {
+      ref: jest.fn()
+    }
+    const eventEmitter = new EventEmitter()
+    database.ref.mockReturnValue(eventEmitter)
+    
+    const wrapper = shallow(<App database={database} />)
+
+    //nao recebeu ainda
+    expect(wrapper.find(Comments).length).toBe(1)
+    expect(wrapper.find(NewComment).length).toBe(1)
+    expect(wrapper.find('p').length).toBe(1)
+  
+    //apos receber values
+    const comments = {
+      'a' : { comment: 'comment1'},
+      'b' : {comment : 'comment2'}
+    }
+    const val = jest.fn()
+    val.mockReturnValue(comments)
+    eventEmitter.emit('value', {
+      val
+    })
+
+    wrapper.update()
+
+    //tests
+    expect(wrapper.state().isLoading).toBeFalsy()
+    expect(wrapper.state().comments).toBe(comments)
+    expect(wrapper.find(Comments).get(0).props.comments).toBe(comments)
+    expect(wrapper.find(NewComment).get(0).props.sendComment).toBe(wrapper.instance().sendComment)
+    expect(wrapper.find('p').length).toBe(0)
+  })
+
 })
