6a3eee76888b4323f5b33712a4872658d6874446
vitor7001 <vitorgomes7001@gmail.com>
2020-01-05 11:56:52 -0300

    Testando o envio de comentários

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
 
-test('renders learn react link', () => {
-/*  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument(); */
-  expect('a').toBe('a')
-});
+import {shallow} from 'enzyme'
+import Comments from './Comments'
+import NewComment from './NewComment'
+
+describe('<App />' , () =>{
+  it('renderizar sem erros', () =>{
+    const database = {
+      ref: jest.fn()
+    }
+    database.ref.mockReturnValue({
+      on : jest.fn()
+    })
+    const wrapper = shallow(<App database={database} />)
+    expect(wrapper.find(Comments).length).toBe(1)
+    expect(wrapper.find(NewComment).length).toBe(1)
+    expect(wrapper.find('p').length).toBe(1)
+  })
+
+
+  it('adicionar um novo comentário', () =>{
+    const database = {
+      ref: jest.fn()
+    }
+    const child = jest.fn()
+    const update = jest.fn()
+    database.ref.mockReturnValue({
+      on : jest.fn(),
+      child,
+      update
+    })
+    const push = jest.fn()
+    child.mockReturnValue({
+      push
+    })
+    push.mockReturnValue({
+      key : '1'
+    })
+    const wrapper = shallow(<App database={database} />)
+    wrapper.instance().sendComment('new comment')
+    expect(child).toBeCalledWith('comments')
+    expect(update).toBeCalledWith({
+      'comments/1' : {'comment' : 'new comment'}
+    })
+  })
+})
